b193fb39df632bdfa7b42e4807a3aedbe290e1a5
Rafael Duarte <elyouus94@gmail.com>
2024-12-29 23:20:15 -0400

    class 11

---
+++ src/TodoIcon.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { ReactComponent as CheckSVG } from "./check.svg";
+import { ReactComponent as DeleteSVG } from "./delete.svg";
+
+const iconTypes = {
+  check: <CheckSVG />,
+  delete: <DeleteSVG />,
+};
+
+function TodoIcon({ type, color }) {
+  return (
+    <span className={`Icon Icon-svg Icon-${type}`}>{iconTypes[type]}</span>
+  );
+}
+
+export { TodoIcon };
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,21 +1,26 @@
 import React from "react";
+import { CompleteIcon } from "./CompleteIcon";
+import { DeleteIcon } from "./DeleteIcon";
 import "./TodoItem.css";
 
 function TodoItem({ text, completed, onCompleted, onDelete }) {
   return (
     <li className="TodoItem">
-      <span
+      <CompleteIcon />
+      {/* <span
         className={`Icon Icon-check ${completed && "Icon-check--active"}`}
         onClick={() => onCompleted(text)}
       >
         V
-      </span>
+      </span> */}
       <p className={`TodoItem-p ${completed && "TodoItem-p--complete"}`}>
         {text}
       </p>
-      <span className="Icon Icon-delete" onClick={() => onDelete(text)}>
+      <DeleteIcon />
+
+      {/* <span className="Icon Icon-delete" onClick={() => onDelete(text)}>
         X
-      </span>
+      </span> */}
     </li>
   );
 }
